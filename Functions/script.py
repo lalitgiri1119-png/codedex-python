fb985156119f77350cde1d79461c20d54961038d
Lalit Ravindra Giri <lalitgiri1119@gmail.com>
2025-11-17 14:04:35 +0530

    Update script.py

--- Functions/script.py
+++ Functions/script.py
@@ -3,22 +3,13 @@
 Oh no! The NASA engineers forgot to convert imperial data to metric data in one of the functions! This noob mistake cost American taxpayers $125 million total.
 
 Suppose you're a NASA intern, and your team is calculating the distance a rocket needs to travel. The value calculated is in kilometers, while the next step requires miles. Your manager quickly writes this on the board:
-
-
 miles= 1.609/kilometer
-​
- 
-
 Create a new file called rocket.py.
-
 Define a function named distance_to_miles() that converts a distance from kilometers to miles. It should:
-
 Take in one parameter named distance (the distance in kilometers).
 Print the distance in miles.
 After, call the function and use 10000 as the argument.'''
-
 # Write code below 💖
-
 def distance_to_miles(distance):
   #distance = int(input("Enter The Distance In Kilometer:"))
   miles=(distance/1.609)
