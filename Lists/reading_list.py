b3e6508c02404ea18cf7b38877d858d07bbb3565
Lalit Giri <lalitgiri1119@gmail.com>
2025-11-17 11:30:08 +0530

    Add reading_list

---
+++ Lists/reading_list.py
@@ -0,0 +1,20 @@
+'''Create a reading_list.py program that stores the following items in a books list:
+
+'Harry Potter'
+'1984'
+'The Fault in Our Stars'
+'The Mom Test'
+'Life in Code'
+Suppose we want to add 'Pachinko' to the list. Can you use a list method to do so?
+
+Let's say we finished reading 'The Fault in Our Stars' and '1984'. Can you use the .remove() method to remove one and the .pop() method to remove the other?
+
+Print the updated list out to make sure everything's good to go!'''
+
+# Write code below 💖
+
+books = ['Harry Potter','1984','The Fault in Our Stars','The Mom Test','Life in Code']
+books.append('Pachinko')
+books.remove('The Fault in Our Stars')
+books.pop(1)
+print(books)
